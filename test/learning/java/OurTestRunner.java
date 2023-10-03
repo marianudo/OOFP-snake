bc663ec5ff0b01e625f1ae53288ca9a566685ddd
Mariano Navas <marianudo@gmail.com>
2023-10-03 18:19:27 +0200

    Prove that using the stateful, mutable implementation of Calculator in a concurrent environment yields an unexpected result.

--- test/learning/java/OurTestRunner.java
+++ test/learning/java/OurTestRunner.java
@@ -1,9 +1,43 @@
 package learning.java;
 
+import static org.junit.Assert.assertEquals;
+
 public class OurTestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO Note for the instructor:
         // Show here what happens when we deal with mutable state in concurrent environments
         // Use for that the CalculatorTest
+        Calculator calc1 = new Calculator();
+        runThreadThatComputes3(calc1);
+        runThreadThatComputes5(calc1);
+        // Wait for the threads to complete calculations
+        Thread.sleep(1000L);
+        assertEquals(3, calc1.getValue(), 0.001);
+    }
+
+    private static void runThreadThatComputes3(Calculator c) {
+        new Thread(() -> calculationsThatReturn3(c), "ThreadReturning3").start();
+    }
+
+    private static void runThreadThatComputes5(Calculator c) {
+        new Thread(() -> calculationsThatReturn5(c), "ThreadReturning5").start();
+    }
+
+    // Will return 3.0 in the Calculator state
+    private static void calculationsThatReturn3(Calculator c) {
+        c.sum(3);
+        c.times(2);
+        c.dividedBy(0.5);
+        c.minus(3);
+        c.sqrt();
+    }
+
+    //Returns 5.0 in the calculator state
+    private static void calculationsThatReturn5(Calculator c) {
+        c.sum(5);
+        c.times(2);
+        c.dividedBy(0.5);
+        c.minus(-5);
+        c.sqrt();
     }
 }
